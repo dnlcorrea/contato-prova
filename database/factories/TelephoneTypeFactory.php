7ef01fdc7fdda268bb2d6e284a8ee213cfda1457
Daniel Correa <daniel@31solutions.com>
2020-12-14 18:57:59 -0300

    1st

---
+++ database/factories/TelephoneTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TelephoneType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TelephoneTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TelephoneType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
